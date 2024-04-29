33709fb78d4e984aff281c26b33260d972538ea3
Your Name <you@example.com>
2024-04-29 20:37:35 +0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import cv2
 from datetime import datetime
 from ultralytics import YOLO
-import requests
 from config import *
 import time
 from mivovo import model_cls
+from service.utils import *
 model = YOLO('ai_models\yolov8n-face.pt')
 
 track = {}
@@ -14,19 +14,12 @@ while True:
     if not cap.isOpened():
         print(f"Error opening video stream for camera {CAM_ID}")
     track = {}
-    emotion_true = 0
-    total = 0
-    age_true = 0
-    gender_true = 0
-    
     while True:
         ret, frame = cap.read()
         if not ret:
             cap.release()
             break
         start_time = datetime.now()
-        # start_time_fps = time.time()  # Start time for FPS calculation
-        i+=1
         results = model.track(frame, persist=True, verbose=False)
         if results[0].boxes.id is None:
             ids = []
@@ -36,98 +29,29 @@ while True:
             boxes = [list(map(int, box)) for box in boxes]
             genders = []
             for box, id in zip(boxes, ids):
-                x, y, x1, y1 = box
-                w, h = x1 - x, y1 - y
-                if w > 60 and h > 60:
-                    face_box = frame[y:y1, x:x1]
+                x1, y1, x2, y2 = box
+                if x2 - x1 > 60 and y2 - y1 > 60:
+                    face_box = frame[y1:y2, x1:x2]
                     age, gender = model_cls.predict(face_box)
                     end_time = datetime.now()
                     genders.append(gender)
                     if track.get(id) is None:
                         track[id] = {}
                         track[id]["tracking_time"] = (end_time - start_time).total_seconds() 
-                        track[id]["age"] = int(age)
-                        track[id]["gender"] = gender
                         track[id]["num_tracking"] = 1
                     else:
                         track[id]["tracking_time"] += (end_time - start_time).total_seconds() 
-                        track[id]["age"] = int(age)
-                        track[id]["gender"] = gender
                         track[id]["num_tracking"] += 1
-                    cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+                    track[id]["age"] = int(age)
+                    track[id]["gender"] = gender
+                    track[id]["box"] = [x1, y1, x2, y2]
+                
+            relationship = get_relationship(genders)
+            report_first_info(track, relationship)
+        track = report_final_info(track, ids)
 
-            if len(set(genders)) > 1:
-                relationship = "couple"
-            else:
-                relationship = "single"
-
-            for key in list(track.keys()):
-                if track[id]["num_tracking"] == 2:
-                    try:
-                        requests.post(
-                            URL_DETECT,
-                            json={
-                                "age": track[key]["age"],
-                                "gender": track[key]["gender"],
-                                "relationship": relationship,
-                            },
-                        )
-                    except:
-                        pass
-
-                cv2.putText(
-                    frame,
-                    str(track[key]["age"]),
-                    (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (36, 255, 12),
-                    2,
-                )
-                cv2.putText(
-                    frame,
-                    track[key]["gender"],
-                    (x, y - 50),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (36, 255, 12),
-                    2,
-                )
-                cv2.putText(
-                    frame,
-                    str(track[key]["tracking_time"]),
-                    (x, y - 70),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (36, 255, 12),
-                    2,
-                )
-                cv2.putText(
-                    frame,
-                    relationship,
-                    (x, y - 90),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (36, 255, 12),
-                    2,
-                )
-
-        for key in list(track.keys()):
-            if key not in ids:
-                try:
-                    requests.post(
-                        URL_LOG,
-                        json={"tracking_time": track[key]["tracking_time"]},
-                    )
-                except:
-                    pass
-                track.pop(key)
-
-        # end_time_fps = time.time()  # End time for FPS calculation
-        # fps = 1 / (end_time_fps - start_time_fps)  # Calculate FPS
-        # print(f"FPS: {fps:.2f}")
-        # print(f"Total: {i}")
         if IS_SHOW:
+            frame = draw(frame, track, relationship)
             cv2.imshow("frame", frame)
             cv2.waitKey(1)
         
--- service/utils.py
+++ service/utils.py
@@ -1,39 +1,83 @@
 import cv2
 import numpy as np
-from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.preprocessing import image
+import requests
+from config import URL_DETECT, URL_LOG
 
+# def preprocess(current_img):
+#     target_size = [224, 224]
+#     if target_size is not None:
+#         factor_0 = target_size[0] / current_img.shape[0]
+#         factor_1 = target_size[1] / current_img.shape[1]
+#         factor = min(factor_0, factor_1)
 
-def preprocess(current_img):
-    target_size = [224, 224]
-    if target_size is not None:
-        factor_0 = target_size[0] / current_img.shape[0]
-        factor_1 = target_size[1] / current_img.shape[1]
-        factor = min(factor_0, factor_1)
+#         dsize = (
+#             int(current_img.shape[1] * factor),
+#             int(current_img.shape[0] * factor),
+#         )
+#         current_img = cv2.resize(current_img, dsize)
 
-        dsize = (
-            int(current_img.shape[1] * factor),
-            int(current_img.shape[0] * factor),
-        )
-        current_img = cv2.resize(current_img, dsize)
+#         diff_0 = target_size[0] - current_img.shape[0]
+#         diff_1 = target_size[1] - current_img.shape[1]
 
-        diff_0 = target_size[0] - current_img.shape[0]
-        diff_1 = target_size[1] - current_img.shape[1]
+#         current_img = np.pad(
+#             current_img,
+#             (
+#                 (diff_0 // 2, diff_0 - diff_0 // 2),
+#                 (diff_1 // 2, diff_1 - diff_1 // 2),
+#                 (0, 0),
+#             ),
+#             "constant",
+#         )
 
-        current_img = np.pad(
-            current_img,
-            (
-                (diff_0 // 2, diff_0 - diff_0 // 2),
-                (diff_1 // 2, diff_1 - diff_1 // 2),
-                (0, 0),
-            ),
-            "constant",
-        )
-
-        # double check: if target image is not still the same size with target.
-        if current_img.shape[0:2] != target_size:
-            current_img = cv2.resize(current_img, target_size)
+#         # double check: if target image is not still the same size with target.
+#         if current_img.shape[0:2] != target_size:
+#             current_img = cv2.resize(current_img, target_size)
         
-        img_pixels = image.img_to_array(current_img)
-        img_pixels = np.expand_dims(img_pixels, axis=0)
-        img_pixels /= 255  # normalize input in [0, 1]
-    return img_pixels
+#         img_pixels = image.img_to_array(current_img)
+#         img_pixels = np.expand_dims(img_pixels, axis=0)
+#         img_pixels /= 255  # normalize input in [0, 1]
+#     return img_pixels
+
+def draw(image, track, relationship):
+    for key, track_info in track.items():
+        cv2.rectangle(image, (track_info["box"][0], track_info["box"][1]), (track_info["box"][2], track_info["box"][3]), (255, 0, 0), 2)
+        cv2.putText(image, str(track_info["age"]), (track_info["box"][0], track_info["box"][1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+        cv2.putText(image, track_info["gender"], (track_info["box"][0], track_info["box"][1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+        cv2.putText(image, str(track_info["tracking_time"]), (track_info["box"][0], track_info["box"][1] - 70), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+        cv2.putText(image, relationship, (track_info["box"][0], track_info["box"][1] - 90), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+    return image
+
+def get_relationship(genders):
+    relationship = "single"
+    if len(set(genders)) > 1:
+        relationship = "couple"    
+    return relationship
+
+def report_first_info(track, relationship):
+    for key in list(track.keys()):
+        if track[key]["num_tracking"] == 2:
+            try:
+                requests.post(
+                    URL_DETECT,
+                    json={
+                        "age": track[key]["age"],
+                        "gender": track[key]["gender"],
+                        "relationship": relationship,
+                    },
+                )
+            except:
+                pass
+
+def report_final_info(track, ids):
+    for key in list(track.keys()):
+        if key not in ids:
+            try:
+                requests.post(
+                    URL_LOG,
+                    json={"tracking_time": track[key]["tracking_time"]},
+                )
+            except:
+                pass
+            track.pop(key)
+    return track
